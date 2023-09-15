2deed651bcce22873e3a439ada59371f60edf722
David <2018dpollack@gmail.com>
2023-09-15 16:56:45 -0700

    Added logos to html

--- src/index.js
+++ src/index.js
@@ -1,32 +1,51 @@
+// array of objects that have a name (team name) and icon (image of logo)
 const teams = [
-    {name: "Hawks", icon: "atlantaHawks.png"},
-    {name: "Celtics", icon: "bostonCeltics.png"},
-    {name: "Nets", icon: "brooklynNets.png"},
-    {name: "Hornets", icon: "charlotteHornets.png"},
-    {name: "Bulls", icon: "chicagoBulls.png"},
-    {name: "Cavs", icon: "clevelandCavaliers.png"},
-    {name: "Mavs", icon: "dallasMavs.png"},
-    {name: "Nuggets", icon: "denverNuggets.png"},
-    {name: "Pistons", icon: "detroitPistons.png"},
-    {name: "Warriors", icon: "goldenStateWarriors.png"},
-    {name: "Rockets", icon: "houstonRockets.png"},
-    {name: "Pacers", icon: "indianaPacers.png"},
-    {name: "Clippers", icon: "LAClippers.png"},
-    {name: "Lakers", icon: "LALakers.png"},
-    {name: "Grizzlies", icon: "memphisGrizzlies.png"},
-    {name: "Heat", icon: "miamiHeat.png"},
-    {name: "Bucks", icon: "milwaukeeBucks.png"},
-    {name: "TimberWolves", icon: "minnesotaTimberWolves.png"},
-    {name: "Pelicans", icon: "NOPelicans.png"},
-    {name: "Knicks", icon: "NYKnicks.png"},
-    {name: "Thunder", icon: "OKCThunder.png"},
-    {name: "Magic", icon: "OrlandoMagic.png"},
-    {name: "Suns", icon: "PheonixSuns.png"},
-    {name: "76ers", icon: "Philadelphia76ers.png"},
-    {name: "TrailBlazers", icon: "portlandTrailBlazers.png"},
-    {name: "Kings", icon: "SacKings.png"},
-    {name: "Spurs", icon: "SASpurs.png"},
-    {name: "Raptors", icon: "torontoRaptors.png"},
-    {name: "Jazz", icon: "utahJazz.png"},
-    {name: "Wizards", icon: "washingtonWizards.png"},
+    {name: "Hawks", logo: "atlantaHawks.png"},
+    {name: "Celtics", logo: "bostonCeltics.png"},
+    {name: "Nets", logo: "brooklynNets.png"},
+    {name: "Hornets", logo: "charlotteHornets.png"},
+    {name: "Bulls", logo: "chicagoBulls.png"},
+    {name: "Cavs", logo: "clevelandCavaliers.png"},
+    {name: "Mavs", logo: "dallasMavs.png"},
+    {name: "Nuggets", logo: "denverNuggets.png"},
+    {name: "Pistons", logo: "detroitPistons.png"},
+    {name: "Warriors", logo: "goldenStateWarriors.png"},
+    {name: "Rockets", logo: "houstonRockets.png"},
+    {name: "Pacers", logo: "indianaPacers.png"},
+    {name: "Clippers", logo: "LAClippers.png"},
+    {name: "Lakers", logo: "LALakers.png"},
+    {name: "Grizzlies", logo: "memphisGrizzlies.png"},
+    {name: "Heat", logo: "miamiHeat.png"},
+    {name: "Bucks", logo: "milwaukeeBucks.png"},
+    {name: "TimberWolves", logo: "minnesotaTimberWolves.png"},
+    {name: "Pelicans", logo: "NOPelicans.png"},
+    {name: "Knicks", logo: "NYKnicks.png"},
+    {name: "Thunder", logo: "OKCThunder.png"},
+    {name: "Magic", logo: "OrlandoMagic.png"},
+    {name: "Suns", logo: "PheonixSuns.png"},
+    {name: "76ers", logo: "Philadelphia76ers.png"},
+    {name: "TrailBlazers", logo: "portlandTrailBlazers.png"},
+    {name: "Kings", logo: "SacKings.png"},
+    {name: "Spurs", logo: "SASpurs.png"},
+    {name: "Raptors", logo: "torontoRaptors.png"},
+    {name: "Jazz", logo: "utahJazz.png"},
+    {name: "Wizards", logo: "washingtonWizards.png"},
 ]
+//This function will create a div for each team in the above array, and create an image object
+//inside each teamDiv. After, it will make the proper children.
+function populateTeams(){
+    const teamLogoDiv = document.querySelector(".team-logos");
+    teams.forEach(team => {
+        const teamDiv = document.createElement("div");
+        teamDiv.classList.add('team-logo');
+
+        const img = document.createElement('img');
+        img.src = `./src/nbaLogos/${team.logo}`;
+        img.alt = team.name;
+
+        teamDiv.appendChild(img);
+        teamLogoDiv.appendChild(teamDiv);
+    })
+}
+
+populateTeams();
