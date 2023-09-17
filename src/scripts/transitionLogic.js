8016e6070d38823531db3b87d7d3d60f05bf8d42
David <2018dpollack@gmail.com>
2023-09-16 17:44:35 -0700

    Icon clicked and goes into middle, but buggy

--- src/scripts/transitionLogic.js
+++ src/scripts/transitionLogic.js
@@ -6,12 +6,26 @@ function selectTeam(){
 
             if (this.classList.contains('enlarged-logo')){
                 this.classList.remove('enlarged-logo');
+                const clonedElement = document.querySelector(".cloned");
+                if (clonedElement){
+                    clonedElement.remove();
+                }
                 return;
             }
 
             teamLogos.forEach(l => l.classList.remove('enlarged-logo'));
 
-            this.classList.add('enlarged-logo');
+            const clone = this.cloneNode(true);
+            // const rect = this.getBoundingClientRect();
+            // clone.style.width = rect.width + 'px';
+            // clone.style.height = rect.height + 'px';
+            clone.classList.add('cloned');
+            document.body.appendChild(clone);
+            setTimeout(() =>{
+                clone.classList.add("enlarged-logo");
+            }, 50);
+            
+            this.classList.add('dissapear');
         });
     });
 }
