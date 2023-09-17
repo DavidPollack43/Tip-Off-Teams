260ddd6c8cc6ab350ec862ab10e0365b35af76c3
David <2018dpollack@gmail.com>
2023-09-16 22:24:43 -0700

    Fixed bug where clone does not get cloned to right size

--- src/scripts/transitionLogic.js
+++ src/scripts/transitionLogic.js
@@ -16,11 +16,24 @@ function selectTeam(){
             teamLogos.forEach(l => l.classList.remove('enlarged-logo'));
 
             const clone = this.cloneNode(true);
-            // const rect = this.getBoundingClientRect();
-            // clone.style.width = rect.width + 'px';
-            // clone.style.height = rect.height + 'px';
+            const rect = this.getBoundingClientRect();
+
+            const originalImage = this.querySelector('img');  // Get the original image
+            const clonedImage = clone.querySelector('img');   // Get the cloned image
+    
+            // Apply the styles directly to the cloned image
+            clonedImage.style.maxWidth = originalImage.style.maxWidth || '100%';
+            clonedImage.style.height = originalImage.style.height || 'auto';
+            clonedImage.style.maxHeight = originalImage.style.maxHeight || '13.5vh';
+
+            clone.style.width = rect.width + 'px';
+            clone.style.height = rect.height + 'px';
+            clone.style.top = rect.top + 'px';
+            clone.style.left = rect.left + 'px';
+
             clone.classList.add('cloned');
             document.body.appendChild(clone);
+
             setTimeout(() =>{
                 clone.classList.add("enlarged-logo");
             }, 50);
