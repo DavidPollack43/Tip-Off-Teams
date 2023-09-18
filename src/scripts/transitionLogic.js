6ce0e17d9db6b53add0371a6b9fd0fa735a49588
David <2018dpollack@gmail.com>
2023-09-18 09:16:09 -0700

    Added comments for readability

--- src/scripts/transitionLogic.js
+++ src/scripts/transitionLogic.js
@@ -1,23 +1,27 @@
+// This funciton will let the user click on a logo to go to that teams page, as well as put
+// in a cool/clean animation that will highlight the team logo and move it to the team page
 function selectTeam(){
-    const teamLogos = document.querySelectorAll(".team-logo");
+    const teamLogos = document.querySelectorAll(".team-logo"); //Get every team logo
 
-    teamLogos.forEach(logo => {
+    const selectTeam = document.querySelector("h2");
+
+    teamLogos.forEach(logo => { //Add a event listner to each logo
         logo.addEventListener("click", function(){
 
-            if (this.classList.contains('enlarged-logo')){
-                this.classList.remove('enlarged-logo');
-                const clonedElement = document.querySelector(".cloned");
-                if (clonedElement){
-                    clonedElement.remove();
-                }
-                return;
-            }
+            teamLogos.forEach(l => l.classList.remove('enlarged-logo')); //Housekeeping step, make sure no other enlarged-logo's exist
 
-            teamLogos.forEach(l => l.classList.remove('enlarged-logo'));
-
+            // Make a clone node. Why? This is because if you don't then you move the original 
+            // logo. This messes up the flex box and shifts every logo. By making a clone
+            // we can shift that clone to become the enlarged logo/team page logo, and just make
+            // the orignal logo dissapear from view, but still exist. 
             const clone = this.cloneNode(true);
-            clone.classList.remove('team-logo');
+            // These two steps ensure that the correct styling is used on the clone
+            clone.classList.remove('team-logo'); 
             clone.classList.add('cloned-team-logo');
+
+            // Get values of the original logo (both dimenstions and styles like margin)
+            // Pivitol beacuse the clone image will get the original, huge bloated image 
+            // dimenstions/styling before putting it into any flexbox/making it smaller
             const rect = this.getBoundingClientRect();
             const computedStyles = window.getComputedStyle(this);
 
@@ -29,26 +33,32 @@ function selectTeam(){
             clonedImage.style.height = originalImage.style.height || 'auto';
             clonedImage.style.maxHeight = originalImage.style.maxHeight || '13.5vh';
 
+            // Apply styles to cloned div
             clone.style.margin = computedStyles.margin;
             clone.style.width = rect.width + 'px';
             clone.style.height = rect.height + 'px';
-            clone.style.top = (rect.top - 10) + 'px'; //Temp fix
-            clone.style.left = (rect.left + 17) + 'px'; //Temp fix
+            clone.style.top = (rect.top - 10) + 'px'; //Temp fix/Does not work on differing screens
+            clone.style.left = (rect.left + 17) + 'px'; //Temp fix/Does not work on differing screens
             clone.style.position = 'absolute';
-
+            
+            // This will put cloned class on the clone, this is so we can reference it later
             clone.classList.add('cloned');
+            //We need to append the child to the document, or else it won't be on the DOM at all
             document.body.appendChild(clone);
 
+            //Here is were we add the enlarged-logo class to the logo
             setTimeout(() =>{
                 clone.classList.add("enlarged-logo");
                 setTimeout(() =>{
-                    clone.style.top = "calc(50% - 50px)";
+                    clone.style.top = "calc(50% - 50px)"; // top/left puts the logo in the middle, specific for my screen for now
                     clone.style.left = "calc(50% + 100px)";
-                    clone.style.transform = "translate(-50%, -50%) scale(4)";
+                    clone.style.transform = "translate(-50%, -50%) scale(4)"; // Right now top left corner in middle, puts center of logo in middle and scales it up 4
+                    teamLogos.forEach(l => l.classList.add('fade-out')); // Makes each logo fade
+                    selectTeam.classList.add('fade-out'); //Makes select a team fade
                 }, 0)
             }, 500);
 
-            this.classList.add('dissapear');
+            this.classList.add('dissapear'); //Makes original logo dissapear.
         });
     });
 }
