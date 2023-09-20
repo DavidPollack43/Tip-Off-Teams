481d9d77afbc085e06173f267ddf15bd0db620d0
David <2018dpollack@gmail.com>
2023-09-20 10:12:49 -0700

    Fixed player mappings

--- frontend/src/scripts/data/playerMapping.js
+++ frontend/src/scripts/data/playerMapping.js
@@ -10,7 +10,7 @@ export const teamStartingFive = {
     "Celtics": [473, 70, 434, 378, 219], //White, Brown, Tatum, Porzingis, Horford
     "Nets": [130, 61, 666679, 417, 666508], //Dinwiddie, Bridges, Johnson, Simmons, Claxton
     "Hornets": [3547239, 403, 111111, 666950, 38017698], //Lamelo, Rozier, Miller(*), Washington, Williams
-    "Bulls": [89, 269, 125, 3547248, 460], //Caruso, Lavine, DeRozan, Willaims, Vucevic
+    "Bulls": [89, 268, 125, 3547248, 460], //Caruso, Lavine, DeRozan, Willaims, Vucevic
     "Cavs": [666581, 322, 666908, 17896076, 9], //Garland, Mitchell, Strus, Mobley, Allen
     "Mavs": [132, 228, 191, 666965, 379], //Doncic, Kyrie, Hardaway, Willaims, Powell
     "Nuggets": [335, 81, 375, 177, 246], //Murray, KCP, MPJ, Gordon, Jokic
@@ -23,9 +23,9 @@ export const teamStartingFive = {
     "Grizzlies": [666786, 420, 3547287, 231, 3], //Morant, Smart, Bane, Jackson, Adams
     "Heat": [666633, 391, 79, 285, 4], //Herro, Richardson, Butler, Love, Bam
     "Bucks": [214, 8, 315, 15, 283], //Jrue, Allen, Middleton, Giannis, Lopez
-    "Timberwolves": [104, 3547238, 3547259, 447, 176], //Conley, ANT, McDaniels, KAT, Gobert
+    "TimberWolves": [104, 3547238, 3547259, 447, 176], //Conley, ANT, McDaniels, KAT, Gobert
     "Pelicans": [303, 227, 18677986, 666969, 455], //McCollum, Murphy, Ingram, Zion, Valanciunas
-    "Knicks": [20, 17895858, 666423, 387, 399], //Brunson, Grimes, RJ, Randle, Mitch
+    "Knicks": [73, 17895858, 666423, 387, 399], //Brunson, Grimes, RJ, Randle, Mitch
     "Thunder": [175, 17896065, 666541, 38017703, 38017685], //Shai, Giddey, Dort, JDub, Chet
     "Magic": [165, 196, 17896026, 38017683, 85], //Fultz, Haris, Wagner, Paolo, Carter Jr.
     "Suns": [37, 57, 356, 140, 22], //Beal, Booker, Okogie, Durant, Ayton
